937499a60d3bc577efea887f9f5cbc1a26b20396
Rohit <rohit.naik246@gmail.com>
2018-04-14 18:35:20 -0400

    config changed

--- arctic-capgen-vid/common.py
+++ arctic-capgen-vid/common.py
@@ -8,10 +8,10 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams
 
 # the dir where there should be a subdir named 'youtube2text_iccv15'
 # RAB_DATASET_BASE_PATH = '/data/lisatmp3/yaoli/datasets/'
-RAB_DATASET_BASE_PATH = '~/video-description-cnn-rnn/arctic-capgen-vid/dataset/'
+RAB_DATASET_BASE_PATH = '/home/rtnaik/arctic-capgen-vid/dataset/'
 # the dir where all the experiment data is dumped.
 # RAB_EXP_PATH = '/data/lisatmp3/yaoli/exp/'
-RAB_EXP_PATH = '~/video-description-cnn-rnn/arctic-capgen-vid/exp/'
+RAB_EXP_PATH = '/home/rtnaik/video-description-cnn-rnn/arctic-capgen-vid/exp/'
 
 def get_two_rngs(seed=None):
     if seed is None:
